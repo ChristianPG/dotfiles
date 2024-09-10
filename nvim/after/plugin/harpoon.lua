db1430db72cdd69ae1369ee0c0a156e7c6667c9a
Christian Patiño <christianpg813@gmail.com>
2024-09-09 21:49:07 -0500

    Update which-key specs

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,14 +1,23 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local mark = require 'harpoon.mark'
+local ui = require 'harpoon.ui'
 
-require('which-key').register {
-	['<leader>o'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+require('which-key').add {
+  { '<leader>o', group = '[H]arpoon' },
+  { '<leader>o_', hidden = true },
 }
 
-vim.keymap.set("n", "<C-q>", ui.toggle_quick_menu)
+vim.keymap.set('n', '<C-q>', ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<leader>ja", mark.add_file, { desc = "Add file to Harpoon" })
-vim.keymap.set("n", "<leader>jj", function() ui.nav_file(1) end, { desc = "Go to first file" })
-vim.keymap.set("n", "<leader>jk", function() ui.nav_file(2) end, { desc = "Go to second file" })
-vim.keymap.set("n", "<leader>jl", function() ui.nav_file(3) end, { desc = "Go to third file" })
-vim.keymap.set("n", "<leader>j;", function() ui.nav_file(4) end, { desc = "Go to fourth file" })
+vim.keymap.set('n', '<leader>ja', mark.add_file, { desc = 'Add file to Harpoon' })
+vim.keymap.set('n', '<leader>jj', function()
+  ui.nav_file(1)
+end, { desc = 'Go to first file' })
+vim.keymap.set('n', '<leader>jk', function()
+  ui.nav_file(2)
+end, { desc = 'Go to second file' })
+vim.keymap.set('n', '<leader>jl', function()
+  ui.nav_file(3)
+end, { desc = 'Go to third file' })
+vim.keymap.set('n', '<leader>j;', function()
+  ui.nav_file(4)
+end, { desc = 'Go to fourth file' })
