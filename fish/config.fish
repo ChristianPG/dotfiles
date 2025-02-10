812322e7fa9fa0ad14433d4d11837109aae688c2
Christian Patiño <christianpg813@gmail.com>
2025-02-10 16:53:14 -0500

    Add python and default node version for the Fish Shell

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set --universal nvm_default_version lts/jod
 nvm use
 # starship init fish | source
 
 alias gitui="gitui -t frappe.ron"
+
+pyenv init - fish | source
